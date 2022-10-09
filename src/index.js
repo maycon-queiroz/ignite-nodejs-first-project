c35211adc749aac300688dfbc570625d2114683e
Maycon Queiroz <fmaycon@gmail.com>
2022-10-09 15:39:13 -0300

    feat(account): add route to create account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-const express = require('express')
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.listen(3333)
+const customers = [];
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const customerAlreadyExist = customers.some((customer) => customer.cpf === cpf);
+
+  if (customerAlreadyExist) {
+    return response.status(400).json({ message: 'Error! customer already exist' });
+  }
+
+  customers.push({
+    id: uuidv4(), name, cpf, statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.get('/', (request, response) => response.status(201).json(customers));
+
+app.listen(3333);
